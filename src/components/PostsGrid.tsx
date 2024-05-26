225715fe427cfe3011bb261633aaae4fc4ab0c59
ywj3493 <ywj3493@naver.com>
2024-05-26 18:46:00 +0900

    💄: 카드 위치 수정

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -7,7 +7,7 @@ type PostsGridProps = {
 
 export default function PostsGrid({ posts }: PostsGridProps) {
   return (
-    <ul className="grid gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+    <ul className="grid gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
         <PostCard key={post.id} data={post} />
       ))}
