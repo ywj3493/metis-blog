4c1ceb31a83073fe2cc91ff7360845ce339cd54d
ywj3493 <ywj3493@naver.com>
2024-05-26 18:45:25 +0900

    🐛: import 버그 픽스

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -1,10 +1,10 @@
-import { getPosts } from "@/api/notion";
+import { getPosts } from "@/services/notion";
 import PostsGrid from "./PostsGrid";
 
 export default async function FeaturedPosts() {
   const posts = await getPosts();
   return (
-    <section className="p-24">
+    <section className="p-24 w-full">
       <h2 className="text-2xl font-bold my-8 px-8">Featured Posts</h2>
       <PostsGrid posts={posts} />
     </section>
