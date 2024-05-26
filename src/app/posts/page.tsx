c90e7686669e53bc374791251e58adcaa26ed3d2
ywj3493 <ywj3493@naver.com>
2024-05-26 19:16:00 +0900

    🔍: 누락된 SEO 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,13 @@
 import FilterablePosts from "@/components/posts/FilterablePosts";
 import { getDatabaseTags, getPosts } from "@/services/notion";
+import { Metadata } from "next";
 
 export const revalidate = 180;
+
+export const metadata: Metadata = {
+  title: "posts",
+  description: "메티의 포스트",
+};
 
 export default async function PostListPage() {
   const posts = await getPosts();
