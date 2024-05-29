e2c00d65f1b65c90b79bc81a60c8588ba8bc704c
ywj3493 <ywj3493@naver.com>
2024-05-29 19:58:55 +0900

    🚑: 삭제 링크 제거

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
       <h2 className="text-3xl font-bold mt-8">{"안녕하세요. 메티입니다."}</h2>
       <h3 className="text-xl font-semibold">웹 프론트엔드 개발자</h3>
       <p>노션 TIL 에서 작성한 것을 공개하는 블로그 입니다.</p>
-      <Link href="/contact">
+      <Link href="/guestbooks">
         <button className="bg-blue font-bold rounded-xl py-4 px-16 text-gray-800">
           Contact
         </button>
