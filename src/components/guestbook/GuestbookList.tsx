5be5974bb1824fb4b87e7723bc7c2a9ee63e8e75
ywj3493 <ywj3493@naver.com>
2024-05-29 16:31:09 +0900

    ⚰️: 불필요한 코드 제거

--- src/components/guestbook/GuestbookList.tsx
+++ src/components/guestbook/GuestbookList.tsx
@@ -3,22 +3,18 @@
 import { useEffect, useState } from "react";
 import GuestbookCard from "./GuestbookCard";
 import GuestbookForm from "./GuestbookForm";
-import Loading from "../Loading";
 import { getGuestbooks } from "@/services/guestbooks";
 
 export default function GuestbookList() {
   const [guestbooks, setGuestbooks] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   const fetchGuestbooks = async () => {
-    setIsLoading(true);
     try {
       const res = await getGuestbooks();
       setGuestbooks(res);
     } catch (e) {
       console.error(e);
     }
-    setIsLoading(false);
   };
 
   useEffect(() => {
