52d1fbfc7f1727e410aea02e058dedf0f8c24996
ywj3493 <ywj3493@naver.com>
2024-06-03 19:55:55 +0900

    🔍: 사이트맵 & 크롤러봇 추가

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+const baseUrl = process.env.BLOG_URL || "";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+const baseUrl = process.env.BLOG_URL || "";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/posts`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/guestbooks`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.8,
+    },
+  ];
+}
