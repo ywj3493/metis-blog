5e2be7f9ef991c5b891adc2047a25e6dc06b33e5
ywj3493 <ywj3493@naver.com>
2024-06-04 14:05:09 +0900

    ✅: 환경변수 추가 및 실제 notion api metadata 에러 테스트 추가

---
+++ src/__test__/services/notion.test.ts
@@ -0,0 +1,26 @@
+// import { server } from "@/mocks/server";
+// import {
+//   getNotionPostMetadata,
+//   getNotionPosts,
+// } from "@/services/_external/notion";
+import { describe, expect, it } from "vitest";
+
+describe("Test for build all posts", () => {
+  it("should load env variables", () => {
+    expect(process.env.NOTION_KEY).toBeDefined();
+    expect(process.env.NOTION_POST_DATABASE_ID).toBeDefined();
+    expect(process.env.NOTION_GUESTBOOK_DATABASE_ID).toBeDefined();
+    expect(process.env.NOTION_USER_ID).toBeDefined();
+    expect(process.env.NOTION_TOKEN_V2).toBeDefined();
+  });
+  // it("should make metadata correctly for all posts", async () => {
+  //   server.close();
+  //   const posts = await getNotionPosts();
+  //   const metadatas = await Promise.all(
+  //     posts.map((post) => getNotionPostMetadata(post.id))
+  //   );
+
+  //   expect(metadatas).toBeDefined();
+  //   server.listen();
+  // });
+});
