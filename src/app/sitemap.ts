ac4c90d54ae75f520e9c492a7558e278ced846d7
ywj3493 <ywj3493@naver.com>
2024-06-20 15:36:47 +0900

    🔍: 게시글 동적 사이트맵 생성

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,16 @@
+import { getNotionPosts } from "@/services/_external/notion";
 import { MetadataRoute } from "next";
 
 const baseUrl = process.env.BLOG_URL || "";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getNotionPosts();
+  const postUrls: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${baseUrl}/posts/${post.id}`,
+    lastModified: new Date(post.last_edited_time),
+    changeFrequency: "daily",
+    priority: 0.8,
+  }));
   return [
     {
       url: baseUrl,
@@ -28,5 +36,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "always",
       priority: 0.8,
     },
+    ...postUrls,
   ];
 }
