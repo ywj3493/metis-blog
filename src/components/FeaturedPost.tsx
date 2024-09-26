9c31fceaf6d826e938a25d22289d8781677bf831
ywj3493 <ywj3493@naver.com>
2024-09-26 21:59:08 +0900

    💄: 어색한 제목 제거

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -7,7 +7,6 @@ export default async function FeaturedPosts() {
   const posts = dataList.map(Post.create);
   return (
     <section className="p-24 w-full">
-      <h2 className="text-2xl font-bold my-8 px-8">Featured Posts</h2>
       <PostsGrid posts={posts} />
     </section>
   );
