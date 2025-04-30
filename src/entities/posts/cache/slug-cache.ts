6ce3ed6f1f5449097bff2369ce3b1f02042b97dd
ywj3493 <ywj3493@naver.com>
2025-04-30 19:07:57 +0900

    ⚡️ 리액트 cache api 로 getSlugMap 캐싱

--- src/entities/posts/cache/slug-cache.ts
+++ src/entities/posts/cache/slug-cache.ts
@@ -1,40 +1,17 @@
+import { Post } from "@/entities/posts/model";
 import { getNotionPosts } from "@/features/notion/model";
-import { Post } from "@/features/posts/model";
+import { cache } from "react";
 
-let cachedSlugMap: Record<string, string> | null = null;
-let lastFetchedAt = 0;
+export const getSlugMap = cache(async () => {
+  const posts = (await getNotionPosts()).map(Post.create);
 
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
-const TTL = 1000 * 60 * 3; // 3분, ISR 주기와 통일
+  const slugMap = posts.reduce(
+    (acc, { id, slugifiedTitle }) => {
+      acc[slugifiedTitle] = id;
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
 
-export async function getSlugMap() {
-  const now = Date.now();
-
-  if (cachedSlugMap && now - lastFetchedAt < TTL) {
-    return cachedSlugMap;
-  }
-
-  const isVercelBuild =
-    process.env.VERCEL === "1" &&
-    process.env.NEXT_PHASE === "phase-production-build";
-
-  if (isVercelBuild) {
-    console.log("[BUILD MODE] Using Notion API directly.");
-    const posts = (await getNotionPosts()).map(Post.create);
-
-    cachedSlugMap = posts.reduce(
-      (acc, { id, slugifiedTitle }) => {
-        acc[slugifiedTitle] = id;
-        return acc;
-      },
-      {} as Record<string, string>,
-    );
-  } else {
-    console.log("[RUNTIME] Fetching slugMap from API...");
-    const res = await fetch(`${BASE_URL}/api/slugs`);
-    if (!res.ok) throw new Error("Failed to fetch slug map");
-    cachedSlugMap = await res.json();
-  }
-  lastFetchedAt = now;
-  return cachedSlugMap;
-}
+  return slugMap;
+});
